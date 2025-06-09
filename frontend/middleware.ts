dd00be686e80af6a357747a072ada13407a7a19f
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-06-09 20:47:09 +0530

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -38,12 +38,11 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
+     * Match all request paths except for:
+     * - Next.js static files (_next/static, _next/image)
+     * - favicon.ico
+     * - static files with extensions like png, jpg, svg, css, js, etc.
      */
-    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|svg|gif|ico|css|js|woff2|woff|ttf|map)$).*)',
   ],
-}; 
+};
